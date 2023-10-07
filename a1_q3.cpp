6cf01fa467b758e21a1f6b7b37905be077dac8d3
nidhimeena-14 <146980424+nidhimeena-14@users.noreply.github.com>
2023-10-07 20:37:38 +0530

    I answered question 3 of assignment 1

---
+++ a1_q3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+bool isPrime(int number) {
+    if (number <= 1) {
+        return false; 
+    }
+
+    if (number == 2) {
+        return true;
+    }
+
+    if (number % 2 == 0) {
+        return false;
+    }
+
+    for (int i = 3; i <= sqrt(number); i += 2) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+
+    if (isPrime(num)) {
+        std::cout << num << " is a prime number." << std::endl;
+    } else {
+        std::cout << num << " is not a prime number." << std::endl;
+    }
+
+    return 0;
+}
